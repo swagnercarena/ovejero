fixed bug in p_dlt_std calculation

ovejero/bnn_inference.py
@@ -469,7 +469,7 @@ class InferenceClass:
 				p_images_jn[ji,pi] = np.mean(samp_p_dlt<=percent)
 		# Estimate the standard deviation from the jacknife
 		p_dlt_std = np.sqrt((len(self.p_dlt)-1)*np.mean(np.square(p_images_jn-
-			np.mean(p_images_jn,axis=0))))
+			np.mean(p_images_jn,axis=0)),axis=0))
 		plt.plot(percentages,p_images,c=color_map[1])
 		# Plot the 1 sigma contours from the jacknife estimate to get an idea of
 		# our sample variance.