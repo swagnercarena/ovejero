b3729073663037abbaf9c4bf2f58d75b73ddad3b
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2020-01-31 13:51:18 -0800

    added shifted and submit script

---
+++ hierarchical_results/submit_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# h_inf.sbatch
+#
+#SBATCH -J h_inf         # A single job name for the array
+#SBATCH -p kipac,normal,iric,hns 
+#SBATCH -n 4              
+#SBATCH -N 1              
+#SBATCH -t 0-24         # Running time of 1 day
+#SBATCH --mem 8000
+#SBATCH -o /home/users/swagnerc/slurm_out/h_inf_%j.out # Standard output
+#SBATCH -e /home/users/swagnerc/slurm_out/h_inf_%j.err # Standard error
+
+cd /home/users/swagnerc/Phil/ovejero/hierarchical_results
+python run_hierarchical_inference.py ${1} ${2} ${3} ${4} ${5} ${6} ${7}
