6fc492d2f276de243dec9c455607eb61a56379d7
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2020-02-03 16:15:26 -0800

    changed the submit script so that it installs everything

--- hierarchical_results/submit_script.sh
+++ hierarchical_results/submit_script.sh
@@ -11,5 +11,14 @@
 #SBATCH -o /home/users/swagnerc/slurm_out/h_inf_%j.out # Standard output
 #SBATCH -e /home/users/swagnerc/slurm_out/h_inf_%j.err # Standard error
 
+ml python/3.6.1
+ml gcc/9.1.0
+cd /home/users/swagnerc/Phil/fastell4py
+python setup.py install --user
+cd /home/users/swagnerc/Phil/baobab
+pip3 install -e . -r requirements.txt
+pip3 install tensorflow==2.1.0 --user
+cd /home/users/swagnerc/Phil/ovejero
+python setup.py install --user
 cd /home/users/swagnerc/Phil/ovejero/hierarchical_results
-python run_hierarchical_inference.py ${1} ${2} ${3} ${4} ${5} ${6} ${7}
+python3 run_hierarchical_inference.py ${1} ${2} ${3} ${4} ${5} ${6} ${7}
