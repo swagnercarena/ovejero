e556b76d9d383dbbc7a9acf7dee2cfbfe199e5cc
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2020-06-24 04:58:29 -0700

    update .sh script as well

--- hierarchical_results/submit_script.sh
+++ hierarchical_results/submit_script.sh
@@ -3,9 +3,9 @@
 # h_inf.sbatch
 #
 #SBATCH -J h_inf         # A single job name for the array
-#SBATCH -p kipac,normal,iric,hns 
-#SBATCH -c 16              
-#SBATCH -N 1              
+#SBATCH -p kipac,normal,iric,hns
+#SBATCH -c 16
+#SBATCH -N 1
 #SBATCH -t 0-24         # Running time of 1 day
 #SBATCH --mem 8000
 #SBATCH -o /home/users/swagnerc/slurm_out/h_inf_%j.out # Standard output
@@ -18,4 +18,4 @@ cd /home/users/swagnerc/Phil/ovejero
 python3 setup.py install --user
 ml python/3.6.1
 cd /home/users/swagnerc/Phil/ovejero/hierarchical_results
-python3 -m run_hierarchical_inference ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8}
+python3 -m run_hierarchical_inference ${1} ${2} ${3} ${4} ${5} ${6} ${7} ${8} ${9}
