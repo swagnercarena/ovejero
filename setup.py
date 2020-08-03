59e42ed7e2d859b77aeaf913ae838ad273080f0e
Sebastian Wagner-Carena <swagnerc@stanford.edu>
2020-08-03 14:45:40 -0700

    updated setup file

--- setup.py
+++ setup.py
@@ -13,20 +13,22 @@ readme = open("README.rst").read()
 PACKAGE_PATH = os.path.abspath(os.path.join(__file__, os.pardir))
 
 required_packages = ['numpy>=1.13', 'scipy>=0.14.0', 'configparser',
-	'pandas>=0.24.2','corner>=2.0.1','tensorflow-probability>=0.8.0']
+	'pandas>=0.24.2','corner>=2.0.1','tensorflow-probability>=0.8.0',
+	'tensorflow>=2.1.0','matplotlib>=3.3.0','numba>=0.48.0',
+	'tdqm>=4.42.1','emcee>=3.0.2']
 
 setup(
-    name='ovejero',
-    version='0.0.1',
-    description='Strong lens Bayesian Neural Network package.',
-    long_description=readme,
-    author='Sebastian Wagner-Carena',
-    author_email='sebaswagner@outlook.com',
-    url='https://github.com/swagnercarena/ovejero',
-    packages=find_packages(PACKAGE_PATH),
-    package_dir={'ovejero': 'ovejero'},
-    include_package_data=True,
-    install_requires=required_packages,
-    license='MIT',
-    zip_safe=False
+	name='ovejero',
+	version='0.0.1',
+	description='Strong lens Bayesian Neural Network package.',
+	long_description=readme,
+	author='Sebastian Wagner-Carena',
+	author_email='sebaswagner@outlook.com',
+	url='https://github.com/swagnercarena/ovejero',
+	packages=find_packages(PACKAGE_PATH),
+	package_dir={'ovejero': 'ovejero'},
+	include_package_data=True,
+	install_requires=required_packages,
+	license='MIT',
+	zip_safe=False
 )
